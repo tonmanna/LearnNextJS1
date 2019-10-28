25f60d98c5bde00b1f7d465bd099951250696020
Worawut Boontan <worawut@theiconweb.com>
2019-10-28 22:37:54 +0700

    remove next folder

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
-import React from "react";
-
-const Home = props => {
-  return <div>Rendering content for domain: {props.renderFrom}</div>;
-};
-
-export default Home;
+import React, { Component } from "react";
+const fetch = require("node-fetch");
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+  static async getInitialProps({ Component, router, ctx }) {
+    var response = await fetch(
+      "https://my-json-server.typicode.com/typicode/demo/posts"
+    );
+    const result = await response.json();
+    return {
+      result
+    };
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <ul></ul>
+      </div>
+    );
+  }
+}
+export default App;
