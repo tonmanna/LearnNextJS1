b849170c50bd76768ff8a8439d4396bcd2586429
Worawut Boontan <worawut@theiconweb.com>
2019-10-29 02:11:10 +0700

    Finish First Lab

---
+++ README.md
@@ -0,0 +1,10 @@
+React PropType
+
+https://www.storyblok.com/tp/next-js-react-guide
+https://www.babelcoder.com/blog/posts/next-9
+https://www.storyblok.com/tp/next-js-react-guide
+https://gist.github.com/whoisryosuke/d034d3eaa0556e86349fb2634788a7a1
+https://nextjs.org/docs#amp-first-page
+
+Dynamic Component
+https://dev.to/arpit016/dynamic-components-in-react-4iic
--- pages/about.js
+++ pages/about.js
@@ -2,31 +2,30 @@ import React, { Component } from "react";
 import "../style.less";
 import Layout from "../components/layout";
 const fetch = require("node-fetch");
-class App extends Component {
+class AboutPage extends Component {
   static async getInitialProps() {
     var response = await fetch(
       "https://my-json-server.typicode.com/typicode/demo/posts"
     );
     const result = await response.json();
     return {
-      result
+      result,
+      about: "about"
     };
   }
 
   render() {
-    console.log(this.props);
-    let test = {
-      DomainID: "111",
-      DomainName: "222"
-    };
     return (
       <div>
         <Layout></Layout>
-        <ul>DomainID : {test.DomainID}</ul>
-        <ul>DomainName : {test.DomainName}</ul>
-        <div className="example">Hello Less!</div>
+        <div className="example">About</div>
+        {this.props.result.map(value => (
+          <div>
+            <label>Title: {value.title}</label> :<label>ID: {value.id}</label>
+          </div>
+        ))}
       </div>
     );
   }
 }
-export default App;
+export default AboutPage;
